154d88dcb5a5708d47517e711a8b76f96830df12
Timo Staudinger <staudinger.timo@gmail.com>
2016-06-02 21:31:44 -0400

    Update strava api version

--- index.js
+++ index.js
@@ -3,19 +3,22 @@
 const runtasticStravaSync = require('./runtastic-strava-sync')
 const config = require('./config.json')
 
-const accounts = config.accounts
-  .map((account) => {
-    const id = new Buffer(account.user).toString('base64')
-    return {
-      user: account.user,
-      password: account.password,
-      stravaAccessToken: account.stravaAccessToken,
-      id: id,
-    }
-  })
+const accounts = config.accounts.map((account) => {
+  const id = new Buffer(account.user).toString('base64')
+  return {
+    user: account.user,
+    password: account.password,
+    stravaAccessToken: account.stravaAccessToken,
+    id: id,
+  }
+})
 
-setInterval(() => {
+function runSync() {
   accounts.map((account) => {
     runtasticStravaSync(account)
   })
-}, 10 * 60 * 1000)
+
+  setTimeout(runSync, 10 * 60 * 1000)
+}
+
+runSync()
